cd98e58604838a5a3954ca0ea4d33ed0d9d8b7bf
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2023-02-08 20:49:15 +0100

    Added Middleware (cors,body-parsing) & app.listen

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = app;
 
@@ -8,18 +10,21 @@ module.exports = app;
 */
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
+//Middleware for handling CORS requests from index.html
+app.use(cors())
 
+// Middware for parsing request bodies:
 
-// Add middware for parsing request bodies here:
+app.use(bodyParser.json())
 
-
-// Mount your existing apiRouter below at the '/api' path.
+// Mount  apiRouter.
 const apiRouter = require('./server/api');
-
+app.use('./api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
-  // Add your code to start the server listening at PORT below:
-
+  // Add code to start the server listening at PORT below:
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+})
 }

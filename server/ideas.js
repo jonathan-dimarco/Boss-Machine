5ae886ede6f13a9a8f9174c8a3b5789521bb58a0
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2023-02-17 20:30:10 +0100

    added functionality to check M.D.Ideas

--- server/ideas.js
+++ server/ideas.js
@@ -9,6 +9,8 @@ const {
     deleteFromDatabasebyId,
   } = require('./db');
 
+  const checkMillionDollarIdea = require('./checkMillionDollarIdea')
+
 ideasRouter.param('ideaId', (req, res, next, id) => {
     const index = Number(id)
     const idea = getFromDatabaseById('ideas', index);
@@ -24,7 +26,7 @@ ideasRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('ideas'))
 })
 
-ideasRouter.post('/', (req, res, next) => {
+ideasRouter.post('/', checkMillionDollarIdea, (req, res, next) => {
     const newIdea = addToDatabase('ideas', req.body);
     res.status(201).send(newIdea);
 })
@@ -33,7 +35,7 @@ ideasRouter.get('/:ideaId', (req, res, next) => {
     res.send(req.idea);
 })
 
-ideasRouter.put('/:ideaId', (req, res, next) =>{
+ideasRouter.put('/:ideaId', checkMillionDollarIdea, (req, res, next) =>{
 const updatedIdea = updateInstanceInDatabase('ideas', req.body);
 res.send(updatedIdea);
 });
